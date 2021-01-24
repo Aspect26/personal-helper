64d30c2b86c407c6a03ad3e4f63c6b2c35c9c118
Julius Flimmel <flimmelj@gmail.com>
2021-01-24 10:14:41 +0100

    Project and data update

--- app/src/main/java/com/example/knowledgebase/db/hardcoded/data/beerDb.kt
+++ app/src/main/java/com/example/knowledgebase/db/hardcoded/data/beerDb.kt
@@ -18,10 +18,10 @@ const val beerDbData: String = """
         "degrees": "12",
         "percentage": "4.9",
         "bottleSize": { "label": "Bottle size", "value": "500" },
-        "year": "2020"
+        "year": "2020",
+        "country": "CZE", 
       }
     },
-
     {
       "title": "IKEA",
       "subtitle": "ÖL LJUS LAGER",
@@ -32,7 +32,8 @@ const val beerDbData: String = """
       "properties": {
         "percentage": "4.7",
         "bottleSize": { "label": "Bottle size", "value": "330" },
-        "year": "2020"
+        "year": "2020",
+        "country": "SWE",
       }
     },
     {
@@ -45,7 +46,8 @@ const val beerDbData: String = """
       "properties": {
         "percentage": "2.5",
         "bottleSize": { "label": "Bottle size", "value": "250" },
-        "year": "2020"
+        "year": "2020",
+        "country": "BEL"
       }
     },
     {
@@ -58,7 +60,8 @@ const val beerDbData: String = """
       "properties": {
         "percentage": "3.9",
         "bottleSize": { "label": "Bottle size", "value": "500" },
-        "year": "2020"
+        "year": "2020",
+        "country": "CZE",
       }
     },
     {
@@ -71,7 +74,8 @@ const val beerDbData: String = """
       "properties": {
         "percentage": "3.2",
         "bottleSize": { "label": "Bottle size", "value": "250" },
-        "year": "2020"
+        "year": "2020",
+        "country": "BEL",
       }
     },
     {
@@ -84,7 +88,8 @@ const val beerDbData: String = """
       "properties": {
         "percentage": "4.6",
         "bottleSize": { "label": "Bottle size", "value": "500" },
-        "year": "2020"
+        "year": "2020",
+        "country": "CZE",
       }
     },
     {
@@ -98,9 +103,129 @@ const val beerDbData: String = """
         "degrees": "11",
         "percentage": "4.8",
         "bottleSize": { "label": "Bottle size", "value": "500" },
-        "year": "2020"
+        "year": "2020",
+        "country": "CZE",
+      }
+    },
+    {
+      "title": "Velkopopovický Kozel",
+      "subtitle": "11",
+      "image": "",
+      "tagImages": [
+        base64:iVBORw0KGgoAAAANSUhEUgAAAGQAAABDCAMAAABdlVDoAAAAS1BMVEXXFBrTFh1sL1D02tz53d6DkK4VRH02PGs3PGt0LEx1LEuyHSyzHCzTFRvUFRzXFBoRRX6Ior8WSYE+aJZjX4TT3efriYz6+/z///80Wqe8AAAABnRSTlPU1en4+PtAtsV6AAAA6ElEQVR42rXPuVVDMQAAQQH2BwxG4rCh/0oJN1Yw28C8HeenE2+s79ufbqz19fnrkdUMRJqBSDMQaQYizUikGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYfUz327XeR4vc7tNpHnt1kAaQMhbTCkDYe0wZA2HNIGQ9pwSBsMacMhbTCkDYe0wZA2HNIGRo5LGwp5eZ8TIW18TIe0wZA2HNIGQ9pwSBsMacMhbTCkDYe04ZA2INIGRNqASBsQeXwYvH/aEdiCmtytLgAAAABJRU5ErkJggg==
+      ],
+      "properties": {
+        "degrees": "11",
+        "percentage": "4.6",
+        "bottleSize": { "label": "Bottle size", "value": "500" },
+        "year": "2020",
+        "country": "CZE",
+      }
+    },
+    {
+      "title": "Amstel",
+      "subtitle": "Bock",
+      "image": "",
+      "tagImages": [],
+      "properties": {
+        "percentage": "7",
+        "bottleSize": { "label": "Bottle size", "value": "300" },
+        "year": "2020",
+        "country": "NLD"
+      }
+    },
+    {
+      "title": "ГАЙДА",
+      "subtitle": "Premium",
+      "image": "",
+      "tagImages": [],
+      "properties": {
+        "degrees": "11.4",
+        "percentage": "4.8",
+        "IBU": "35",
+        "bottleSize": { "label": "Bottle size", "value": "500" },
+        "year": "2020",
+        "country": "BGR"
+      }
+    },
+    {
+      "title": "Litovel",
+      "subtitle": "ALE",
+      "image": "",
+      "tagImages": [
+        "base64:iVBORw0KGgoAAAANSUhEUgAAAGQAAABDCAMAAABdlVDoAAAAS1BMVEXXFBrTFh1sL1D02tz53d6DkK4VRH02PGs3PGt0LEx1LEuyHSyzHCzTFRvUFRzXFBoRRX6Ior8WSYE+aJZjX4TT3efriYz6+/z///80Wqe8AAAABnRSTlPU1en4+PtAtsV6AAAA6ElEQVR42rXPuVVDMQAAQQH2BwxG4rCh/0oJN1Yw28C8HeenE2+s79ufbqz19fnrkdUMRJqBSDMQaQYizUikGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYfUz327XeR4vc7tNpHnt1kAaQMhbTCkDYe0wZA2HNIGQ9pwSBsMacMhbTCkDYe0wZA2HNIGRo5LGwp5eZ8TIW18TIe0wZA2HNIGQ9pwSBsMacMhbTCkDYe04ZA2INIGRNqASBsQeXwYvH/aEdiCmtytLgAAAABJRU5ErkJggg=="
+      ],
+      "properties": {
+        "percentage": "5.5",
+        "type": "Ale",
+        "bottleSize": { "label": "Bottle size", "value": "500" },
+        "year": "2020",
+        "country": "CZE"
+      }
+    },
+    {
+      "title": "Staropramen",
+      "subtitle": "DÉČKO",
+      "image": "",
+      "tagImages": [
+        "base64:iVBORw0KGgoAAAANSUhEUgAAAGQAAABDCAMAAABdlVDoAAAAS1BMVEXXFBrTFh1sL1D02tz53d6DkK4VRH02PGs3PGt0LEx1LEuyHSyzHCzTFRvUFRzXFBoRRX6Ior8WSYE+aJZjX4TT3efriYz6+/z///80Wqe8AAAABnRSTlPU1en4+PtAtsV6AAAA6ElEQVR42rXPuVVDMQAAQQH2BwxG4rCh/0oJN1Yw28C8HeenE2+s79ufbqz19fnrkdUMRJqBSDMQaQYizUikGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYfUz327XeR4vc7tNpHnt1kAaQMhbTCkDYe0wZA2HNIGQ9pwSBsMacMhbTCkDYe0wZA2HNIGRo5LGwp5eZ8TIW18TIe0wZA2HNIGQ9pwSBsMacMhbTCkDYe04ZA2INIGRNqASBsQeXwYvH/aEdiCmtytLgAAAABJRU5ErkJggg=="
+      ],
+      "properties": {
+        "percentage": "4.0",
+        "bottleSize": { "label": "Bottle size", "value": "500" },
+        "year": "2020",
+        "country": "CZE"
+      }
+    },
+    {
+      "title": "Argus",
+      "subtitle": "Granát",
+      "image": "",
+      "tagImages": [
+        "base64:iVBORw0KGgoAAAANSUhEUgAAAGQAAABDCAMAAABdlVDoAAAAS1BMVEXXFBrTFh1sL1D02tz53d6DkK4VRH02PGs3PGt0LEx1LEuyHSyzHCzTFRvUFRzXFBoRRX6Ior8WSYE+aJZjX4TT3efriYz6+/z///80Wqe8AAAABnRSTlPU1en4+PtAtsV6AAAA6ElEQVR42rXPuVVDMQAAQQH2BwxG4rCh/0oJN1Yw28C8HeenE2+s79ufbqz19fnrkdUMRJqBSDMQaQYizUikGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYfUz327XeR4vc7tNpHnt1kAaQMhbTCkDYe0wZA2HNIGQ9pwSBsMacMhbTCkDYe0wZA2HNIGRo5LGwp5eZ8TIW18TIe0wZA2HNIGQ9pwSBsMacMhbTCkDYe04ZA2INIGRNqASBsQeXwYvH/aEdiCmtytLgAAAABJRU5ErkJggg=="
+      ],
+      "properties": {
+        "degrees": "11",
+        "percentage": "4.3",
+        "type": "Semi dark",
+        "bottleSize": { "label": "Bottle size", "value": "500" },
+        "year": "2020",
+        "country": "CZE"
+      }
+    },
+    {
+      "title": "Litovel",
+      "subtitle": "Sváteční speciál",
+      "image": "",
+      "tagImages": [
+        "base64:iVBORw0KGgoAAAANSUhEUgAAAGQAAABDCAMAAABdlVDoAAAAS1BMVEXXFBrTFh1sL1D02tz53d6DkK4VRH02PGs3PGt0LEx1LEuyHSyzHCzTFRvUFRzXFBoRRX6Ior8WSYE+aJZjX4TT3efriYz6+/z///80Wqe8AAAABnRSTlPU1en4+PtAtsV6AAAA6ElEQVR42rXPuVVDMQAAQQH2BwxG4rCh/0oJN1Yw28C8HeenE2+s79ufbqz19fnrkdUMRJqBSDMQaQYizUikGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYk0Q5FmJNIMRZqRSDMUaUYizVCkGYfUz327XeR4vc7tNpHnt1kAaQMhbTCkDYe0wZA2HNIGQ9pwSBsMacMhbTCkDYe0wZA2HNIGRo5LGwp5eZ8TIW18TIe0wZA2HNIGQ9pwSBsMacMhbTCkDYe04ZA2INIGRNqASBsQeXwYvH/aEdiCmtytLgAAAABJRU5ErkJggg=="
+      ],
+      "properties": {
+        "degrees": "13",
+        "percentage": "6.0",
+        "bottleSize": { "label": "Bottle size", "value": "500" },
+        "year": "2020",
+        "country": "CZE"
       }
     }
   ]
 }
+"""
+
+private val templateItem = """
+    {
+      "title": "template",
+      "subtitle": "",
+      "image": "",
+      "tagImages": [],
+      "properties": {
+        "degrees": "",
+        "percentage": "",
+        "type": "",
+        "IBU": "",
+        "bottleSize": { "label": "Bottle size", "value": "" },
+        "year": "",
+        "country": ""
+      }
+    }
 """
